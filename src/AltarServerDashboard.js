fcbe1f66714b39d9a1f47e705461c210fe97c912
Totsisko <franxcamina@gmail.com>
2024-09-10 18:02:03 +0800

    Add files via upload

---
+++ src/AltarServerDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MenuButton from './MenuButton'; // Import the MenuButton component
+
+const AltarServerDashboard = () => {
+  return (
+    <div>
+      <MenuButton /> {/* Add MenuButton at the top */}
+      <h1>Altar Server Dashboard</h1>
+      {/* Add more content for Altar Server here */}
+    </div>
+  );
+};
+
+export default AltarServerDashboard;
